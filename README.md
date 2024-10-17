7432b189b2d2ac2886386889b33d38f0e9ac35fd
Mike Langmayr <1809691+mikelangmayr@users.noreply.github.com>
2024-10-17 11:01:06 -0700

    Coo-64 Initial WDLFILES refactor (#1)
    * Build a collection of wdl files, move Makefile to a python script, add CI workflow for each wdl src
    
    * isntall gpp in github workflow
    
    * make submodules available in gh workflow
    
    * install numpy to gh workflow
    
    * install matplotlib to gh workflow
    
    * install scipy to gh workflow
    
    * update to latest gh python version
    
    * force node 20 in gh workflow
    
    * undo node version
    
    * update README, run build script on all src folders
    
    * fix gh workflow
    
    * update README
    
    * automatically push newly built or changed .acf files
    
    * Automated ACF files update by GitHub Actions
    
    * only push ACF for one python version
    
    * only push ACF for one python version
    
    * fix gh workflow
    
    * gh workflow: only commit and push if there are changes
    
    * add nirc2 ACF files
    
    * Automated ACF files update by GitHub Actions
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,63 @@
+# wdlfiles
+
+## Overview
+
+The `wdlfiles` repository is dedicated to storing and archiving WDL (Waveform Definition Language) source files. These files are used to generate `.acf` files, which are firmware files for the STA Archon camera controller. This repository facilitates the conversion of WDL source files to `.acf` files using scripts from the [CaltechOpticalObservatories/wdl](https://github.com/CaltechOpticalObservatories/wdl) repository.
+
+Additionally, CI workflows are set up to automate the conversion process, ensuring that the stored WDL files can be successfully converted to `.acf` files.
+
+## Key Features
+
+- **WDL File Storage:** Store and archive multiple versions of WDL source files.
+- **Automated Conversion:** Use scripts from the [wdl](https://github.com/CaltechOpticalObservatories/wdl) repository to convert WDL files to `.acf` firmware files.
+- **CI Integration:** Continuous Integration (CI) is implemented to automatically run the conversion process and verify that the WDL files are valid and can be compiled into `.acf` files.
+
+## Repository Structure
+
+The repository is structured as follows:
+
+```
+wdlfiles/ 
+├── src/ # Folder containing the WDL source files 
+├── acf/ # Folder for storing generated .acf files 
+├── .github/ # CI workflows for automating conversion 
+└── README.md # This documentation file
+```
+
+## Prerequisites
+
+### System Requirements:
+- Python 3.x
+- GNU Make
+- GPP (GNU Preprocessor) [Download GPP](https://logological.org/gpp)
+
+### Python Packages:
+- `numpy`
+- `scipy`
+- `matplotlib`
+- `pyqt`
+
+### WDL
+- Ensure you have the necessary dependencies and environment to run the ACF build script    (refer to the [wdl repository](https://github.com/CaltechOpticalObservatories/wdl) for setup instructions).
+
+
+## Steps to Convert WDL to ACF
+
+1. Place the WDL source files in the `src/` directory of this repository.
+2. Run the ACF build script in the `bin` folder to generate `.acf` files.
+```
+    bin/build_acf.py {target}
+```
+3. The `.acf` files will be stored in the `acf/` directory for further use.
+
+## CI Workflow
+
+The CI pipeline is configured to:
+
+- Trigger on every push to the repository.
+- Automatically run the ACF build script for the WDL files stored in the `src/` folder.
+- Report success or failure based on whether the conversion process completes successfully.
+
+## Contributing
+
+Feel free to submit pull requests with new WDL source files or improvements to the conversion process. Please ensure that all new files are compatible with the CI pipeline.
---
+++ bin/build_acf.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import subprocess
+import re
+
+# Please set the correct gpp path here
+GPP = "/usr/bin/gpp"
+
+CURDIR = os.path.dirname(os.path.abspath(__file__))
+WDLPATH = f"{CURDIR}/../wdl/wdl"
+PLOT = "False"  # Default, set to True to show waveform plots
+GFLAGS = '+c "/*" "*/" +c "//" "\\n"'
+
+SEQPARSER = f"{WDLPATH}/seqParserDriver.py"
+INCPARSER = f"{WDLPATH}/incParserDriver.py"
+WDLPARSER = f"{WDLPATH}/wdlParserDriver.py"
+MODPARSER = f"{WDLPATH}/modParserDriver.py"
+WAVGEN = f"{WDLPATH}/wavgenDriver.py"
+MODEGEN = f"{WDLPATH}/modegenDriver.py"
+I2A = f"{WDLPATH}/ini2acf.pl"
+
+def read_config_variables(file_path):
+    variables = {}
+    
+    # Define the regex pattern to match lines with variable assignments
+    pattern = re.compile(r'(\w+)\s*=\s*"([^"]+)"')
+
+    # Open the file and read line by line
+    with open(file_path, 'r') as file:
+        for line in file:
+            # Match the pattern
+            match = pattern.search(line)
+            if match:
+                # Extract the variable name and value
+                variable_name = match.group(1)
+                variable_value = match.group(2)
+                variables[variable_name] = variable_value
+
+    return variables
+
+def get_src_dir(target):
+    return f'{CURDIR}/../src/{target}'
+
+def scan_file_for_key(target, key):
+    try:
+        config_variables = read_config_variables(f"{get_src_dir(target)}/{target}.conf")
+        return f"{get_src_dir(target)}/{config_variables[key]}"
+    except FileNotFoundError:
+        print(f"{target}.conf does not exist")
+        return None
+
+
+def run_command(command):
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    if result.returncode != 0:
+        print(f"Error: {result.stderr}")
+        exit(1)
+    return result.stdout
+
+
+def build_acf(target):
+    f_tmp = f"{get_src_dir(target)}/{target}_TMP"
+    
+    # Scan for MODULE_FILE
+    modfile = scan_file_for_key(target, "MODULE_FILE")
+    print(f"Looking for MODULE_FILE = {modfile} ...")
+    if not modfile or not os.path.isfile(modfile):
+        print(f"{modfile} does not exist")
+        return
+
+    print(f"Making {f_tmp}.wdl from {target}.conf ...")
+    run_command(f"cat {get_src_dir(target)}/{target}.conf | {SEQPARSER} - | {GPP} {GFLAGS} -I{get_src_dir(target)} | {WDLPARSER} - > {f_tmp}.wdl")
+
+    print(f"Making {f_tmp}.script, {f_tmp}.states from {f_tmp}.wdl ...")
+    run_command(f"echo {f_tmp} | cat  - {modfile} | {GPP} {GFLAGS} -I{get_src_dir(target)} | {MODPARSER} -")
+    
+    print(f"Running WAVGEN for {f_tmp} ...")
+    run_command(f"{WAVGEN} {f_tmp} {PLOT}")
+
+    print(f"Assembling {target}.acf ...")
+    
+    # Scan for CDS_FILE
+    cdsfile = scan_file_for_key(target, "CDS_FILE")
+    print(f"Looking for CDS_FILE = {cdsfile} ...")
+    if not cdsfile or not os.path.isfile(cdsfile):
+        print(f"{cdsfile} does not exist")
+        return
+
+    run_command(f"cat {target}.conf | {INCPARSER} - | cat - {cdsfile} | {GPP} {GFLAGS} -I{get_src_dir(target)} | "
+                f"cat - {f_tmp}.script {f_tmp}.modules {f_tmp}.states {f_tmp}.system | {I2A} - > {get_src_dir(target)}/{target}.acf")
+
+    # Scan for MODE_FILE
+    modefile = scan_file_for_key(target, "MODE_FILE")
+    run_command(f"{MODEGEN} {modefile} {get_src_dir(target)}/{target}.acf")
+
+    # Insert REV keyword if in git
+    if os.path.isdir(".git"):
+        print("Inserting REV keyword ...")
+        run_command(f"{WDLPATH}/insert_hash {get_src_dir(target)}/{target}.acf")
+    else:
+        print("Not a git archive, skipping REV keyword")
+
+    print(f"Removing {get_src_dir(target)}/{target}_TMP files ...")
+    run_command(f"rm -f {get_src_dir(target)}/{target}_TMP.*")
+
+    print(f"Moving {get_src_dir(target)}/{target}.acf file to acf folder ...")
+    run_command(f"mv {get_src_dir(target)}/{target}.acf {CURDIR}/../acf/")
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        build_acf(sys.argv[1])
+    else:
+        print("No target argument provided.")
