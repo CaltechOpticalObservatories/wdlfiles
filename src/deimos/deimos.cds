011018b7e69542de17ccb30ff4c0910cd34e4e93
meiyu <pgupta@caltech.edu>
2026-09-25 14:55:21 -0700

    Cleanup wdl source files

---
+++ src/deimos/deimos.cds
@@ -0,0 +1,146 @@
+/* -*- C -*- */
+/** ---------------------------------------------------------------------------
+ * @file   wasp.cds
+ * @brief  CDS/Deinterlace parameters for WaSP instrument
+ */
+
+#include "voltage_timing_parameters.h"
+
+
+//attempt pixel timing coincident with the nearest ADM samples. That means multiples of 8
+//by eye, try 80 to start
+// then it looks like 13 ADM samples, 80 + 13 *8 = 184
+
+  //signal is 13 ADM samples, starting from time 232
+  //232 + 13 * 8 = 338
+  
+  // last sample number must fit the series 8*n-1
+  // first sample number must be in the series 8*n
+BIGBUF        = _ARCHON_FRAMEBUFS  
+RAWSTARTLINE  = 0
+  //to view the last prescan and start of the line
+  //RAWSTARTPIXEL = 48
+  //to view the end of the line
+RAWSTARTPIXEL = 1061
+SAMPLEMODE    = 1
+RAWENABLE     = _RAW_ENABLE
+RAWENDLINE    = 800
+RAWSAMPLES    = 20000
+  
+
+  //ADM module installed in slot 7
+
+
+  // NOTE tghere is a re-mapping due to the cameralink cable,
+  // that is not accounted for by the current DEIMOS VIB.
+  // currently, SCI 2E channel is connected to FCS L1 (which will be channel 9 in the ADM card, tap channel 45)
+  // raw channel selection 49
+  // SCI 2F channel is connected to SCI 4f (which will be channel 8 in the ADM card, tap channel 44)
+  // note this should also be inverted in principle, not sure if that's actually ahppenbing TBD
+  // raw channel selection 48
+
+
+  //NOTE due to a small mistake in the VIB schematic, ALL signals come out inverted. Due to a mistake in the mapping of the cameralink cable, the channels are mixed up and some are inverted. This inversion cancels out the other inversion. Hence SCI2F is NOT inverted, but SCI 2E IS
+
+  
+
+#if 0
+  //detector currently installed in slot 2
+TAPLINE0="AM39L,1,100"
+TAPLINE1="AM40R,1,100"
+TAPLINES=2
+FRAMEMODE=0
+
+BIGBUF=_ARCHON_FRAMEBUFS
+LINECOUNT=_LINENUM
+PIXELCOUNT=_AMPREADCOLS
+  //RAWSEL        = _RAW_SELECT
+  //NOTE RAWSEL of 11 should be E channel of slot 2
+RAWSEL=11
+RAWSTARTLINE=0
+  //to view the last prescan and start of the line
+  //RAWSTARTPIXEL = 48
+  //to view the end of the line
+
+SHP1=120
+SHP2=303
+SHD1=448
+SHD2=575
+
+#elif 0
+TAPLINES=2
+FRAMEMODE=0
+TAPLINE0="AM45L,1,100"
+TAPLINE1="AM46R,1,100"  
+
+RAWSEL=48
+LINECOUNT  = _FCS_LINENUM
+PIXELCOUNT = _FCS_TOTAL_COLS  
+
+SHP1 = 350
+SHP2 = 500
+SHD1 = 600
+SHD2 = 750
+
+RAWSTARTPIXEL=23
+  
+  
+#else
+  //FRAMEMODE=0
+/* TAPLINE0="AM37L,1,100" */
+/* TAPLINE1="AM38R,1,100" */
+/* TAPLINE2="AM39L,1,100" */
+/* TAPLINE3="AM40R,1,100" */
+/* TAPLINE4="AM41L,1,100" */
+/* TAPLINE5="AM42R,1,100" */
+/* TAPLINE6="AM43L,1,100" */
+/* TAPLINE7="AM44R,1,100" */
+/* TAPLINE8="AM47L,1,100" */
+/* TAPLINE9="AM48R,1,100" */
+/* TAPLINE10="AM49L,1,100" */
+/* TAPLINE11="AM50R,1,100" */
+/* TAPLINE12="AM51L,1,100" */
+/* TAPLINE13="AM52R,1,100" */
+/* TAPLINE14="AM53L,1,100" */
+  //TAPLINE15="AM54R,1,100"
+  //TAPLINES=16
+
+FRAMEMODE=2
+TAPLINE0="AM37L,1,100"
+TAPLINE1="AM38R,1,100"
+TAPLINE2="AM39L,1,100"
+TAPLINE3="AM40R,1,100"
+TAPLINE4="AM41L,1,100"
+TAPLINE5="AM42R,1,100"
+TAPLINE6="AM43L,1,100"
+TAPLINE7="AM44R,1,100"
+TAPLINE8="AM54L,1,100"
+TAPLINE9="AM53R,1,100"
+TAPLINE10="AM52L,1,100"
+TAPLINE11="AM51R,1,100"
+TAPLINE12="AM50L,1,100"
+TAPLINE13="AM49R,1,100"
+TAPLINE14="AM48L,1,100"
+TAPLINE15="AM47R,1,100"
+TAPLINES=16
+
+  
+LINECOUNT=_LINENUM
+PIXELCOUNT=_AMPREADCOLS
+RAWSEL        = _RAW_SELECT
+  //NOTE RAWSEL of 11 should be E channel of slot 2
+RAWSEL        = 11
+
+SHP1          = 120
+SHP2          = 303
+SHD1          = 448
+SHD2          = 575
+
+  
+  
+#endif
+
+TRIGOUTFORCE=0
+TRIGOUTINVERT=0
+TRIGOUTLEVEL=0
+TRIGOUTPOWER=1
---
+++ src/deimos/voltage_timing_parameters.h
@@ -0,0 +1,160 @@
+/** ---------------------------------------------------------------------------
+ * Define clock voltage levels here (units are in volts)
+ * All voltages are set relative to front substrate voltage (typ. 0V)
+ * evaluations relative to FSS should be implemented if variability is required
+/*/     
+
+#define _PAR_CLOCK_HIGH     10.0 /* [ 8.00, 14.0] */
+#define _PAR_CLOCK_LOW     0.0 /* [-0.50, 0.50] */
+
+#define _TG_CLOCK_HIGH      10.5 /* [ 8.00, 14.0] */
+#define _TG_CLOCK_LOW       0.0 /* [-0.50, 0.50] */
+
+#define _SER_CLOCK_HIGH    11.0 /* [ 8.00, 14.0] */
+#define _SER_CLOCK_LOW     1.0 /* [-0.50, 1.50] */
+#define _SER_CLOCK_RCV     11.5 /* Higher than serial clock high */
+
+#define _RESET_DRAIN       17 /* [ 15.0, 20.0] */
+#define _OUTPUT_DRAIN      29 /* [ 27.0, 32.0] */
+
+#define _RG_LOW            5.0 /* [-0.5, 1.0] */
+#define _RG_HIGH           12.0 /* [8.0, 14.0] */
+
+#define _SW_LOW            _SER_CLOCK_LOW /* [-0.5, 1.0] */
+#define _SW_HIGH           _SER_CLOCK_HIGH /* [8.0, 14.0] */
+
+
+/** ---------------------------------------------------------------------------
+ * Define timing parameters here (units are in ticks - 10ns)
+ * Evaluations are used for specific timing parameters and slew rates
+/*/ 
+
+
+#define clockfreq  100000000                      /* 100 MHz master clock frequency in Hz */
+#define sec        *(clockfreq)                   /* clock cycles per second              */
+#define ms         *(clockfreq/1000)              /* clock cycles per millisec            */
+#define us         *(clockfreq/1000000)           /* clock cycles per microsec            */
+#define ns         *(clockfreq/10000000)          /* clock cycles per nanosecond          */
+#define clicks	   *(clockfreq/100000000)	      /* clock cycles per nanosecond          */
+
+/* Timing defines */
+/* Generic timing parameters */
+#define TICK	      #eval 1 clicks		  /* 10 nsec */
+#define 1ns           #eval 1 ns 
+#define 1us           #eval 1 us
+#define 2us           #eval 2 us
+#define 10us          #eval 10 us
+#define 20us          #eval 20 us
+#define 25us          #eval 25 us
+#define 1ms	      #eval 100000 clicks	  /* 999 usec WHY */
+#define 10ms          #eval 10 ms
+#define 1s            #eval 1000 ms
+
+
+/* Line transfer timing definitions */
+#define TDRT 20us
+#define TOI  20us
+#define TDTR 20us
+
+#define TGDELAY 20us
+#define SERTRANSDELAY 10us
+
+
+
+/* Serial transfer timing definitions */
+
+#define SCLK_RCV_RESET #eval 60 TICK /* Serial clock recieving to reset */
+#define TWX #eval 8 TICK /* Reset Pulse Width    */
+#define TOR #eval 56 TICK  /* Serial Clock Overlap */
+#define TSW #eval Pixel_T/2 TICK 
+#define TSR #eval Pixel_T/3 TICK
+
+/** basic constants to assist in somewhat automating slew rate for triange clocking.
+    For now we are just entering a slew time manually, then calculating the rate to make
+    sure we hit the intended voltage at the top of the triangle waveform **/
+
+/** basic slew rate logic: you are doing 1024 serial transfer cycles (for each amp), each lasing around 1us. In this time 3 parallel clock transfers happen, up and down once each. Therefore in 2048us we need to do 3 slews from top to bottom, hence the slew time is 1024 / 6 = 171 */
+
+#define Pixel_T       1000 /* Full pixel time : 10s of ns */
+#define SER_SLEW_TIME_TICKS        333
+
+#define SER_SLEW_TIME_US          3.33
+
+#defeval PAR_SLEW_TIME_US          ((2048)/2 + 50 + 20) * (Pixel_T / 100)   / 3
+#define PAR_SLEW_TIME_TICKS       PAR_SLEW_TIME_US * 100
+
+
+
+//NOTE: OH DEAR, does GPP not do floating point calculations?
+//UGLY HACK: for now we call a command line calculator to do it
+
+//NOTE archon slew rates are defined in Volts per microsecond, NOT in volts per Archon clock tick
+//NOTE waveforms currently use parallel SLOW slew rate to mean triangular clock,
+// and FAST to mean immediate clock
+
+//these exec constructs only work on bash shell, and therefore fail on ubuntu where /bin/sh
+//is still INEXPLICABLY dash rather than bash. wah wah.
+//#define P_TRI_SLEW_RATE    #exec printf '%2.1f' $(bc  <<< "scale=2; (_PAR_CLOCK_HIGH - _PAR_CLOCK_LOW) / PAR_SLEW_TIME_US")
+//#define S_TRI_SLEW_RATE    #exec printf '%2.1f' $(bc  <<< "scale=2; (_SER_CLOCK_HIGH - _SER_CLOCK_LOW) / SER_SLEW_TIME_US")
+#define P_TRI_SLEW_RATE    #exec printf "%2.4f" $(echo "scale=4; (_PAR_CLOCK_HIGH - _PAR_CLOCK_LOW) / PAR_SLEW_TIME_US" | bc)
+#define S_TRI_SLEW_RATE    #exec printf "%2.4f" $(echo "scale=4; (_SER_CLOCK_HIGH - _SER_CLOCK_LOW) / SER_SLEW_TIME_US" | bc)
+
+
+
+
+#define PCLK_slow           P_TRI_SLEW_RATE
+
+#define PCLK_slow_FCS       PCLK_slow
+
+
+//e2v says need on average 1.us rise time, for "normal" clocking calculate PCLK_fast that way
+
+#define PCLK_fast           #exec printf "%2.4f" $(echo "scale=4; (_PAR_CLOCK_HIGH - _PAR_CLOCK_LOW) / 1.5" | bc)
+
+
+
+
+//e2v says need on average 90ns rise time for a serial clock
+#define SCLK_fast           #exec printf "%2.4f" $(echo "scale=4; (_SER_CLOCK_HIGH - _SER_CLOCK_LOW) / 0.2" | bc)
+
+#defeval SCLK_fast_FCS SCLK_fast
+
+//NOTE: waveforms currently use serial "slow" slew rate for triangular waveform
+// serial "FAST" is for immediate changes (like e.g. resetting serial register)
+#define SCLK_slow           S_TRI_SLEW_RATE   //nominal value for now
+
+#defeval SCLK_slow_FCS       SCLK_slow
+
+//transfer gate uses only one slew rate
+
+#define TG_fast              PCLK_fast
+#define TG_slow              12
+
+
+#define NOISETEST 0
+#define CLAMPTEST 0
+#define RESETTEST 0
+
+
+//FCS related voltage definitions
+
+#define _PAR_CLOCK_HIGH_FCS 7.0
+#define _PAR_CLOCK_LOW_FCS -6.0
+#define _MPP_CLOCK_HIGH_FCS 7.5
+#define _MPP_CLOCK_LOW_FCS -5.0
+#define _SER_CLOCK_HIGH_FCS 8.0
+#define _SER_CLOCK_LOW_FCS -5.0
+
+#define _RG_LOW_FCS 2.0
+#define _RG_HIGH_FCS 12.0
+
+#define _SW_LOW_FCS -2.0
+#define _SW_HIGH_FCS 6.0
+
+#define _RESET_DRAIN_FCS 14.0
+#define _OUTPUT_DRAIN_FCS 24.3
+#define _LASTGATE_FCS -4.0
+#define _DUMP_DRAIN_FCS 20.0
+
+
+#define PCLK_fast_FCS  #exec printf "%2.4f" $(echo "scale=4; (_PAR_CLOCK_HIGH_FCS - _PAR_CLOCK_LOW_FCS) / 3.0" | bc)
